38813166bb50af238d9a657006bfb4bfe60bcad9
Om Veerwal <omveerwal9@gmail.com>
2025-07-15 09:07:33 +0530

    filter task and stored to local storage

--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
-const TaskList = ({ tasks, deleteTask }) => {
+const TaskList = ({  deleteTask , onComplete , filterTodo }) => {
+
     return (
         <div className='mt-4 row row-cols-1 row-cols-md-2 g-4'>
-            {tasks.map((task, index) => (
+            
+            {filterTodo.map((task, index) => (
                 <div className='col' key={index}>
                     <div className='card p-3'>
-                        <h3>{task}</h3>
+                        <p style={{textDecoration : task.completed ? 'line-through' : 'none' }}>
+                            {task.text}
+                        </p>
                         <button className='btn btn-danger' onClick={() => deleteTask(index)}>
                             Delete
+                        </button>
+                        <button className='btn btn-success'onClick={()=>onComplete(index)}>
+                            {task.completed ? 'undo' : 'compelete'}
                         </button>
                     </div>
                 </div>
